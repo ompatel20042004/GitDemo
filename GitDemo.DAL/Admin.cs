e3eca73ecb5c651c958ac746a6aee3ea9d2141b0
TheGitDemo <patelom20042004@gmail.com>
2025-03-24 12:49:57 +0530

    All models

---
+++ GitDemo.DAL/Admin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDemo.DAL
+{
+    public class Admin
+    {
+        public Guid AdminId { get; set; }
+        public string AdminName { get; set; }
+        public string AdminEmail { get; set; }
+        public string AdminPassword { get; set; }
+    }
+}
---
+++ GitDemo.DAL/Common.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDemo.DAL
+{
+    public class Common
+    {
+        public string CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string UpdatedBy { get; set; }
+        
+        public DateTime UpdatedOn { get; set; }
+        public string DeletedBy { get; set; }
+        public DateTime DeletedOn { get; set; }
+    }
+}
